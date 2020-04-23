e119d47c744f9ff180fb5680d5e42c4af59355b1
shubham9019 <shubham9019@gmail.com>
2020-04-23 14:50:15 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import sys
+import os
+import signal
+import time
+from subprocess import *
+from tkinter import messagebox
+
+
+
+def file_previous_close():
+    try:
+        with open('home_id.txt', 'r') as f:
+            lines = f.read().splitlines()
+            last_line = lines[-1]
+            os.kill(int(last_line),signal.SIGKILL)
+    except:
+        print('first instance no need to close previous file')
+#file_previous_close()
+
+def writing_id():
+    file_home_id=open("home_id.txt","w+")
+    home_id=os.getpid()
+    file_home_id.writelines('home\n')
+    file_home_id.writelines(str(home_id))
+    file_home_id.close()
+    print(home_id)
+
+writing_id()
